a9c2f8fca97bfdca5e330f53f87d967fb2582f5e
Felipe Botero <59348629+FeBotero@users.noreply.github.com>
2022-06-27 00:25:15 -0400

    Update Subscribe.tsx

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -3,6 +3,7 @@ import { useState,FormEvent} from "react";
 import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
 import { useCreateSubscriberMutation } from "../graphql/generated";
+import ScreenMockup from "../assets/code-mockup.png"
 
 
 export function Subscribe(){
@@ -63,7 +64,7 @@ export function Subscribe(){
 
                 </div>
             </div>
-            <img src="/src/assets/code-mockup.png" alt="" className="mt-10" />
+            <img src={ScreenMockup} alt="" className="mt-10" />
         </div>
     )
 }
